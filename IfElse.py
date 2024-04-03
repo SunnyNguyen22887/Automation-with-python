f8572f8cde08e9a9cf69a97ca007f97598682746
SunnyNguyen22887 <153810207+SunnyNguyen22887@users.noreply.github.com>
2024-04-03 09:27:37 +0700

    Add files via upload

---
+++ IfElse.py
@@ -0,0 +1,17 @@
+greeting = "Hello How are you?"
+if greeting == "Hello":
+    print("condition do not match")
+    print("xin chao")
+elif greeting == "Hello How are you?":
+    print("Condition matching")
+    print("Xin chao, dao nay ban khoe khong?")
+else:
+    print("done if else control flow")
+print("close")
+
+a = 10
+b = 20
+if a > b:
+    print(a)
+else:
+    print(b)
