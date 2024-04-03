2cda49f0025eb8bbb6554b28881c9f9841fbcf3d
SunnyNguyen22887 <153810207+SunnyNguyen22887@users.noreply.github.com>
2024-04-03 09:28:43 +0700

    Add files via upload

---
+++ FirstDemo.py
@@ -0,0 +1,10 @@
+b, c, d = 5, 6.4, "Great"
+print("{} {}".format("values is", b))
+print(type(b))
+
+print("{} {}".format("values is", c))
+print(type(c))
+
+print("{} {}".format("values is", d))
+print(type(d))
+
