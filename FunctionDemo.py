4abba98b7b649f3fcb4daff85a0ca21d6c43acba
SunnyNguyen22887 <153810207+SunnyNguyen22887@users.noreply.github.com>
2024-04-03 09:28:27 +0700

    Add files via upload

---
+++ FunctionDemo.py
@@ -0,0 +1,9 @@
+def GreetMe(name):
+    print("good morning " +name)
+# funtion call
+GreetMe("Sunny")
+
+def AddIntegers(a,b):
+    return a+b   #print(a+b)
+
+print(AddIntegers(2,3)) # AddIntegers(10,9)
