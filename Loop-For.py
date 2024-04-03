24d15ea74ac415b8ddd6b405b3e5c3c9e9f7a522
SunnyNguyen22887 <153810207+SunnyNguyen22887@users.noreply.github.com>
2024-04-03 09:25:45 +0700

    Add files via upload

---
+++ Loop-For.py
@@ -0,0 +1,17 @@
+ojb = [2, 3, 5, 7, 9]
+for i in ojb:
+    print(i)
+print("--------------")
+
+sum = 0
+for j in range(1, 6): # j from 1 to j-1
+   sum = sum + j
+   print(sum)
+print("--------------")
+print(sum)
+print("--------------")
+for m in range(10): # 0->9
+    print(m)
+print("--------------")
+for k in range(1, 10, 2): #0-> 9, lấy giá trị đầu + 2 mỗi lần print
+    print(k)
